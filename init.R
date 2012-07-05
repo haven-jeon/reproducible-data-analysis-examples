b6d2eef0f3d58b8bcd5bfff5c67e7faf1b9950f2
Heewon Jeon <madjakarta@gmail.com>
2012-07-05 14:29:58 +0900

    rename to init.R

--- init.R
+++ init.R
@@ -1,23 +1,11 @@
-
-
-
-
 library(knitr)
-
-setwd("d:")
-
-knit("test.Rmd")
-
-
-system("pandoc test.md -o test.docx")
-
-
-
 
 library(devtools)
 install.packages("brew")
 install_github('pander', 'daroczig')
-
 library(pander)
+
+
+knit("data_analysis.Rmd")
 
 
